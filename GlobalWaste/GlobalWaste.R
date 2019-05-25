17964b99487cac3d51f799323c7a6c0a9eaf9bfd
Paul Meiners <paul.meiners@outlook.de>
2019-05-25 16:07:30 +0200

    adding time changes

--- GlobalWaste/GlobalWaste.R
+++ GlobalWaste/GlobalWaste.R
@@ -27,6 +27,8 @@ names(data_all)[12] <- c("waste_total_pcap")
 # select only necessary columns
 data_clean <- select(data_all, waste_mis_total:gdp_pcap, waste_total_pcap, Year, Code)
 
+data_clean <- mutate(data_all, gdp = total_pop*gdp_pcap)
+
 saveRDS(data_clean, file="data_clean.rds")
 
 #--------------------- Exploratory data analysis---------------- #
@@ -48,6 +50,7 @@ ggplot(data=subset(data_clean, !is.na(waste_mis_total)), aes(x=waste_mis_total, 
   # total waste is produced by the large countries. 
   # The relationship is however not direct, the toal waste is lower than you would expect from countries with high coastal population.
 
+# Waste pcap and coastal population
 ggplot(data=subset(data_clean, !is.na(waste_mis_pcap)), aes(x = log1p(waste_mis_pcap), y = log(coast_pop))) + 
   geom_point(aes(color = gdp_pcap), size = 3) +
   geom_text_repel(aes(label=ifelse(log1p(waste_mis_pcap)>0.12,as.character(cntry),'')),
@@ -55,10 +58,60 @@ ggplot(data=subset(data_clean, !is.na(waste_mis_pcap)), aes(x = log1p(waste_mis_
                    point.padding = 0.5,
                    segment.color = 'grey50') +
   scale_color_viridis()
+# Waste pcap and total population
+ggplot(data=subset(data_clean, !is.na(waste_mis_pcap)), aes(x = log1p(waste_mis_pcap), y = log(coast_pop))) + 
+  geom_point(aes(color = gdp_pcap), size = 3) +
+  geom_text_repel(aes(label=ifelse(log1p(waste_mis_pcap)>0.12,as.character(cntry),'')),
+                  box.padding   = 0.35, 
+                  point.padding = 0.5,
+                  segment.color = 'grey50') +
+  scale_color_viridis()
 
   # countries with high waste per capita are mostly Island countries
-  # also: Richer countries generally have low per capita waste, high per capita waste countries have all low gdp per capita
+  # there is no impact of coastal population vs total population here
 
+ggplot(data=subset(data_clean, !is.na(waste_mis_pcap)), aes(x = log(gdp_pcap), y = log(waste_mis_total))) + 
+  geom_point(aes(color = waste_mis_pcap), size = 3) +
+  geom_text_repel(aes(label=ifelse(log1p(waste_mis_pcap)>0.12,as.character(cntry),'')),
+                  box.padding   = 0.35, 
+                  point.padding = 0.5,
+                  segment.color = 'grey50') +
+  geom_smooth()+
+  scale_color_viridis()
+
+  'The countries with high waste per capita are distributed across the spectrum, suggesting other reasons for their high waste levels
+  Maybe Island countries are just more affected (per capita) by waste that others produce
+  The relationship between gdp and waste is nonlinear. Higher gdp per capita seems to increase total waste for the first half of the distriution.
+  For the second half of the distribution, higher gdp per capita is negatively associated with total waste.'
+
+
+# --- Integrating time effects --- #
+
+# GDP per capita change compared to last year:
+data_clean <- data_clean %>%
+  filter(!is.na(gdp_pcap)) %>%
+  group_by(cntry) %>%
+  arrange(Year, .by_group = TRUE) %>%
+  mutate(gdp_pcap_change = (gdp_pcap/lag(gdp_pcap) - 1) * 100)
+
+# Population change compared to last year:
+data_clean <- data_clean %>%
+  filter(!is.na(total_pop)) %>%
+  group_by(cntry) %>%
+  arrange(Year, .by_group = TRUE) %>%
+  mutate(total_pop_change = (total_pop/lag(total_pop) - 1) * 100)
+
+
+
+ggplot(data=subset(data_clean, !is.na(gdp_pcap_change)), aes(x=gdp_pcap_change)) + 
+  geom_histogram(stat="bin")
+
+ggplot(data=subset(data_clean, !is.na(total_pop_change)), aes(x=total_pop_change)) + 
+  geom_histogram(stat="bin")
+
+ggplot(data = data_clean, aes(x = total_pop_change, y = gdp_pcap_change))+
+  geom_point()
+  
 
 
 
