b5135cb6c20d3c6f5477e21ca841ce5492575179
Paul Meiners <paul.meiners@outlook.de>
2019-06-02 13:01:51 +0200

    adding ridge plots

--- WineRatings/wineratings.R
+++ WineRatings/wineratings.R
@@ -7,7 +7,7 @@ library(readr)
 library(tidyverse)
 theme_set(theme_bw())
 library(ggrepel) # to move labels away from information in plots
-
+library(ggridges)
 
 
 
@@ -35,6 +35,37 @@ wine_ratings %>%
   geom_boxplot()
 
 # The boxplot shows that we have a number of extreme outliers. Which wines are that expensive?
+
+filter(wine_ratings, price>1300) %>% select(id, country, points, price, taster_name, winery)
+
+# Except for one case, all of these wines have been judged by Roger Voss, a professional wine taster. 
+# Ignoring these wines creates a much more understandable graph:
+
+wine_ratings %>%
+  filter(!is.na(price), price<1300) %>% 
+  ggplot(mapping = aes(x = points, y = price, group = points))+
+  geom_boxplot()
+
+# Compare countries and prices for wine:
+wine_ratings %>%
+  filter(!is.na(price), price<1300) %>% 
+  ggplot(data = wine_ratings, mapping = aes(x = price, y = country))+
+         geom_density_ridges(alpha = 0.5) +
+   scale_x_log10(limits=c(1,500), breaks = scales::log_breaks(n = 10))
+
+# English wine is more expensive, and Ukrainian wine more cheap than average.
+
+# Compare countries and ratings for wine:
+
+wine_ratings %>%
+  filter(!is.na(price), price<1300) %>% 
+  ggplot(data = wine_ratings, mapping = aes(x = points, y = country))+
+  geom_density_ridges(alpha = 0.5) 
+
+# Wines from Ukraine and Peru seem to get less points than in other countries
+
+
+
 
 
 
